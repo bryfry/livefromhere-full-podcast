fb30cc6cbd26e31a20fa85206ea455d64b2e3505
bryfry <bryon.fryer@gmail.com>
2019-01-05 17:20:27 -0500

    fmt

--- gen_feed.go
+++ gen_feed.go
@@ -1,79 +1,79 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/feeds"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"time"
-	"fmt"
-  "net/http"
-  "io/ioutil"
-  "encoding/json"
 )
 
 type Envelope struct {
-  Items []Item
+	Items []Item
 }
 
 type Item struct {
-   Audio Audio
+	Audio Audio
 }
 
 type Audio struct {
-  Encodings []Encoding
-  Title string
-  ProgramID string `json:"program_id"`
-  UpdatedAt string `json:"updated_at"`
+	Encodings []Encoding
+	Title     string
+	ProgramID string `json:"program_id"`
+	UpdatedAt string `json:"updated_at"`
 }
 
 type Encoding struct {
-  HttpFilePath string `json:"http_file_path"`
+	HttpFilePath string `json:"http_file_path"`
 }
 
 func main() {
 
-  // Fetch data from the live from here website
-  url := "https://www.livefromhere.org"
-  latestShows := url + "/listen/1.json"
-  res, err := http.Get(latestShows)
-  if err != nil {
-    log.Fatal(err)
-  }
-  body, err := ioutil.ReadAll(res.Body)
-  if err != nil {
-    log.Fatal(err)
-  }
+	// Fetch data from the live from here website
+	url := "https://www.livefromhere.org"
+	latestShows := url + "/listen/1.json"
+	res, err := http.Get(latestShows)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  // Marshal the data into our datastructures with only the fields we care about
-  var env Envelope
-  err = json.Unmarshal(body, &env)
-  if err != nil {
-    log.Fatal(err)
-  }
+	// Marshal the data into our datastructures with only the fields we care about
+	var env Envelope
+	err = json.Unmarshal(body, &env)
+	if err != nil {
+		log.Fatal(err)
+	}
 	now := time.Now()
 
-  // Generate the feed from the data 
+	// Generate the feed from the data
 	newfeed := &feeds.AtomFeed{
 		Title:   "Live From Us - Full Shows",
-    Id:      "LiveFromUs-Full",
+		Id:      "LiveFromUs-Full",
 		Link:    &feeds.AtomLink{Href: url},
 		Author:  &feeds.AtomAuthor{AtomPerson: feeds.AtomPerson{Name: "APM", Email: ""}},
-		Updated: now.String(), 
+		Updated: now.String(),
 	}
-  for _, i := range env.Items {
+	for _, i := range env.Items {
 		newentry := &feeds.AtomEntry{
 			Title:   i.Audio.Title,
 			Id:      i.Audio.ProgramID,
-      Updated: i.Audio.UpdatedAt,
-			Links:   []feeds.AtomLink {feeds.AtomLink{
-           Href: i.Audio.Encodings[0].HttpFilePath,
-           Type: "audio/mpeg",
-           Length: "1024",
-           Rel: "enclosure",
-           }},
+			Updated: i.Audio.UpdatedAt,
+			Links: []feeds.AtomLink{feeds.AtomLink{
+				Href:   i.Audio.Encodings[0].HttpFilePath,
+				Type:   "audio/mpeg",
+				Length: "1024",
+				Rel:    "enclosure",
+			}},
 		}
 		newfeed.Entries = append(newfeed.Entries, newentry)
-  }
+	}
 	writer, _ := os.Create("feed.xml")
 	feeds.WriteXML(newfeed, writer)
 	fmt.Println("feed update complete", now)
